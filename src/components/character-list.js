b95f2f3994027aa1185c8ad2728b7068fc646e8e
Glitch (react-live-search-starting-point) <none>
2017-12-11 11:42:15 +0000

    Import of Thinkful-Ed/react-live-search-starting-point

--- src/components/character-list.js
+++ src/components/character-list.js
@@ -3,10 +3,11 @@ import React from 'react';
 export default function CharacterList(props) {
     const characters = props.characters.map((character, index) =>
         <li key={index}>
-            <strong>{character.name}</strong> ({character.actor}) - {character.description}</li>
+            <strong>{character.name}</strong> ({character.actor}) - {character.description}
+        </li>
     );
     return (
-        <ul className="character-list" aria-live="polite">
+        <ul className="character-list">
             {characters}
         </ul>
     );
