9492ca518e8e86e1b485bd09bcc6aaf4eaebc78d
Glitch (react-live-search-starting-point) <none>
2019-01-23 14:32:31 -0600

    Initial commit and turned Live-Search into a class. Currently passes down a callback function to child search-form. Logs out the e.target.value onChange of search bar input.

--- src/components/live-search.js
+++ src/components/live-search.js
@@ -4,12 +4,27 @@ import SearchForm from './search-form';
 import CharacterCount from './character-count';
 import CharacterList from './character-list';
 
-export default function LiveSearch(props) {
+export default class LiveSearch extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            searchTerm: 'Dale',
+            characters: props.characters
+        }
+    }
+
+    setSearchTerm(searchTerm){
+        console.log(`I ran and searchTerm is ${searchTerm}`);
+        this.setState({searchTerm})
+    }
+    //Make a state for the characters, update based on SearchForm
+  render(){
     return (
         <div className="live-search">
-            <SearchForm />
-            <CharacterCount count={props.characters.length} />
-            <CharacterList characters={props.characters} />
+            <SearchForm handleUpdate={input => this.setSearchTerm(input)} />
+            <CharacterCount count={this.state.characters.length} />
+            <CharacterList characters={this.state.characters} />
         </div>
     );
+}
 }
